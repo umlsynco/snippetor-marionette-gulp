a9086c8d0d79250aff8ea21baf52bf6f1151a24a
umlsynco <evgeny.alexeyev@gmail.com>
2016-06-02 17:33:31 +0300

    Add repository description for the snippetor's cached repositories. #27

--- public/js/app/views/github/UserProfile.js
+++ public/js/app/views/github/UserProfile.js
@@ -51,7 +51,8 @@ define(
                 gid : this.model.get("id"), // github id
                 repository :
                     this.model.get("full_name"), // repository full name
-                branch : this.model.get("default_branch") // default branch
+                branch : this.model.get("default_branch"), // default branch
+                description : this.model.get("description") // default branch
               },
               function(err, model) {
                 if (model && model.has("count")) {
@@ -73,6 +74,7 @@ define(
           <svg aria-hidden="true" class="octicon octicon-repo repo-icon" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9h-1v-1h1v1z m0-3h-1v1h1v-1z m0-2h-1v1h1v-1z m0-2h-1v1h1v-1z m8-1v12c0 0.55-0.45 1-1 1H6v2l-1.5-1.5-1.5 1.5V14H1c-0.55 0-1-0.45-1-1V1C0 0.45 0.45 0 1 0h10c0.55 0 1 0.45 1 1z m-1 10H1v2h2v-1h3v1h5V11z m0-10H2v9h9V1z"></path></svg>\
           <span class="repo-and-owner css-truncate-target">\
             <span class="repo" title="node-github"><%= getFullName() %></span>\
+            <span class="repo-description css-truncate-target"><%=getDescription()%></span>\
           </span>\
           <span class="right" lable="Repo followers"><i class="fa fa-eye fw">&nbsp;<%=getFollowCount()%>&nbsp;&nbsp;</i></span>\
           <span id="sp-repo-comments" class="right" lable="Repo snippets"><i class="fa fa-comment fw">&nbsp;<%= getSnippets() %>&nbsp;&nbsp;</i></span>\
@@ -86,6 +88,10 @@ define(
         },
         templateHelpers: function() {
            return {
+               getDescription: function() {
+                  var descr = this["repository"]["description"];
+                  return descr ?  descr : "";
+               },
                getFullName: function() {
                   return this["repository"]["repository"];
                },
